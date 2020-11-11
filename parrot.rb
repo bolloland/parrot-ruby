d147ae115d7b7fa9c0310545a959c0b4e48f00be
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-10 19:56:38 -0600

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-    puts "#{phrase}" 
+    puts "#{phrase}"    #puts the phrase Squawk! with no argument, and Pretty Bird! with that argument
     return "#{phrase}"   
 end
 
